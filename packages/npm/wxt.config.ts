0b87ee6f989b9612a75aa12a52daabc574da6b5d
qqfunc <qqfunc@gmail.com>
2025-11-20 20:17:21 +0900

    chore: reorder WXT configuration

--- packages/npm/wxt.config.ts
+++ packages/npm/wxt.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from 'wxt'
 export default defineConfig({
   srcDir: 'src',
   outDir: 'dist',
-  modules: ['@wxt-dev/webextension-polyfill'],
   manifest: {
     name: 'Omnibox Search for npm',
     short_name: 'Search npm',
     omnibox: { keyword: '@npm' },
   },
+  modules: ['@wxt-dev/webextension-polyfill'],
 })
